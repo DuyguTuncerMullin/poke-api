2c617ed8bbfa909929fae26e140d96aa180988ba
Duygu Tuncer Mullin <duygu.tuncer001@gmail.com>
2023-11-15 17:45:48 -0600

    add form field to submit data for the new users

--- client/src/components/Customers.tsx
+++ client/src/components/Customers.tsx
@@ -5,6 +5,9 @@ import CustomerData from "../interfaces/CustomerData";
 
 const Customers: React.FC = () => {
   const [data, setData] = useState<CustomerData[]>([]);
+  const [name, setName] = useState("");
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     const getDataFromServer = async () => {
@@ -14,12 +17,60 @@ const Customers: React.FC = () => {
     };
     getDataFromServer();
   }, []);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "userName") {
+      setUserName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("http://localhost:4000/api/customers", {
+        name,
+        username,
+        email,
+      });
+      console.log("response", data);
+      console.log("Data submitted successfully!");
+    } catch (error) {
+      console.error("Error submitting data:", error);
+    }
+  };
+
   return (
     <div>
       <div>
-        {data.map(({ _id, name }) => (
-          <li key={_id}>{name}</li>
-        ))}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            value={name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="userName"
+            placeholder="user name"
+            value={username}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="email address"
+            value={email}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Add Your Info</button>
+        </form>
       </div>
     </div>
   );
